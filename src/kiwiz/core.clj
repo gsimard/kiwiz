9ee61e0fab85313b03a07ea3b4920b21215cefb2
Guillaume Simard <guillaume.simard@cephei.ca>
2011-12-21 10:56:10 -0500

    Added usage information to kiwiz and README file.

--- src/kiwiz/core.clj
+++ src/kiwiz/core.clj
@@ -2,6 +2,9 @@
   (:use kiwiz.module kiwiz.libs)
   (:gen-class))
 
+(defn show-usage []
+  (println "Run with a single argument (destination folder). KiWiz will output libraries to the given path. ie.: \"kiwiz .\""))
+
 (defn write-library [path [file library]]
   (spit (str path "/" file)
         (with-out-str
@@ -12,7 +15,9 @@
 
 (defn -main [& args]
   (let [dest (if (= 1 (count args))
-               (first args)
-               ".")]
-    (dorun (map (partial write-library dest) IPC-libraries))
-    (println (str "Libraries written to: " dest "/"))))
+               (first args))]
+    (if dest
+      (do
+        (dorun (map (partial write-library dest) IPC-libraries))
+        (println (str "Libraries written to: " dest "/")))
+      (show-usage))))
