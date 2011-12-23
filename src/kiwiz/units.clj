7de7ebbf7dad95edc0052dd3a625f6da75d58f51
Guillaume Simard <guillaume.simard@polymtl.ca>
2011-12-23 11:38:19 -0500

    Reverted units system to basic numbers, using nm as base unit.

--- src/kiwiz/units.clj
+++ src/kiwiz/units.clj
@@ -1,94 +1,50 @@
 (ns kiwiz.units
   (import java.lang.Math))
 
-(defn- unit? [u]
-  (and
-   (map? u)
-   (every? u [:unit :val])))
-
-;; the nm is the smallest unit to which all
-;; other are brought back to for a conversion
-(defn nm [u]
-  {:pre [(or (number? u) (unit? u))]}
-  (let [n (if (unit? u)
-            (cond
-             (= :decimils (:unit u)) (* (:val u) 2540)
-             (= :mils (:unit u)) (* (:val u) 25400)
-             (= :inches (:unit u)) (* (:val u) 25400000)
-             (= :mm (:unit u)) (* (:val u) 1000000)
-             (= :nm (:unit u)) (:val u)
-             true 0)
-            u)]
-    {:unit :nm :val (int n)}))
-
-(defn- unit-cast [u unit factor]
-  {:pre [(or (number? u) (unit? u))]}
-  (let [v (if (unit? u)
-            (/ (:val (nm u)) factor)
-            u)]
-    {:unit unit :val v}))
-
-(defn- unit-test [u unit]
-  (and
-   (unit? u)
-   (= unit (:unit u))))
-
-(defn nm? [u]
-  (unit-test u :nm))
-
-;; units in .mod files are decimils
-(defn decimils [u]
-  (unit-cast u :decimils 2540))
-(defn decimils? [u]
-  (unit-test u :decimils))
-
-;; converts mils to decimils
-(defn mils [u]
-  (unit-cast u :mils 25400))
-(defn mils? [u]
-  (unit-test u :mils))
-
-;; converts inches to decimils
-(defn inches [u]
-  (unit-cast u :inches 25400000))
-(defn mils? [u]
-  (unit-test u :inches))
-
-;; converts mm to decimils
-(defn mm [u]
-  (unit-cast u :mm 1000000))
-(defn mm? [u]
-  (unit-test u :mm))
-
 ;; 1 dmil / 10000 dmils/inch * 0.0254 m/inch * 1e9 nm/m = 2540 nm/dmil
 ;; 1 mil / 1000 mils/inch * 0.0254 m/inch * 1e9 nm/m = 25400 nm/mil
 ;; 1 inch * 0.0254 m/inch * 1e9 nm/m = 25400000 nm/inch
 ;; 1 mm / 1000 mm/m * 1e9 nm/m = 1e6 nm/mm
 
-(defn add [x y]
-  {:pre [(and
-          (or (number? x) (unit? x))
-          (or (number? y) (unit? y)))]}
-  (nm (+ (:val (nm x)) (:val (nm y)))))
+;; the nm is the smallest unit to which all
+;; other are brought back to for a conversion
+(defn nm [n]
+  {:pre [(number? n)]}
+  (int n))
+(defn to-nm [n]
+  {:pre [(number? n)]}
+  (int n))
 
-(= [3 2 1] (reverse [1 2 3]))
+(defn mm [n]
+  {:pre [(number? n)]}
+  (* n 1000000))
+(defn to-mm [n]
+  {:pre [(number? n)]}
+  (/ n 1000000))
 
-(sequential? {:a 1 :b 2})
+;; decimils are the units used in .mod files
+(defn decimils [n]
+  {:pre [(number? n)]}
+  (* n 2540))
+(defn to-decimils [n]
+  {:pre [(number? n)]}
+  (/ n 2540))
 
-;; (decimils? (decimils (mm 34)))
+(defn mils [n]
+  {:pre [(number? n)]}
+  (* n 25400))
+(defn to-mils [n]
+  {:pre [(number? n)]}
+  (/ n 25400))
 
-;; (nm (mm 34))
+(defn inches [n]
+  {:pre [(number? n)]}
+  (* n 25400000))
+(defn to-inches [n]
+  {:pre [(number? n)]}
+  (/ n 25400000))
 
-;; (nm (mils 1))
 
-;; (mils? (mils (mm 1)))
 
-;; (nm? (nm (nm 2)))
 
-;; (inches (mils 1))
 
-;; (nm 121.9)
-
-;; (mm? (nm (mm 1.2)))
-
-;; (inches (mm 1))
